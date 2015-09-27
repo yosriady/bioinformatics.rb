f92b4cfce85e48ed4e0db1be0e90f39b711dab32
Yos Riady <yosriady@gmail.com>
2015-09-27 18:54:55 +0800

    Work on AFFXFilter

---
+++ GeneExpressionProfileClassification/AFFXFilter.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+# Given a directory of .txt samples, should filters out AFFX control genes from all samples
+
+input_dir = '/Users/yosriady/Downloads/Assignment2-GeneExpression/training/trainset-txt'
+output_dir = '/Users/yosriady/Downloads/Assignment2-GeneExpression/training/trainset-txt-affx-cleaned'
+
+def filter_control_gene(input, output)
+  line_num = 1
+  File.open(output, 'w') do |output| # 'w' for a new file, 'a' append to existing
+    File.open(input, 'r').each do |line|
+      if line_num > 4
+        is_control_gene = line.start_with?('AFFX')
+        if !is_control_gene
+          output.write(line)
+        end
+      end
+      line_num += 1
+    end
+  end
+end
+
+def filter_control_genes(input_dir, output_dir)
+  Dir.foreach(input_dir) do |item|
+    next unless item.end_with? '.TXT'
+    input = "#{input_dir}/#{item}"
+    output = "#{output_dir}/#{item}"
+    filter_control_gene(input, output)
+  end
+end
+
+filter_control_genes(input_dir, output_dir)
+p 'Done!'
